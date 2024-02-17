8149e0397dcb8cf0e9ee42f96a4d2958ca953a69
Ling Su <26560603+LingSu-dev@users.noreply.github.com>
2024-02-17 12:51:04 -0500

    Update main.py
    Forgot to remove print tests

--- main.py
+++ main.py
@@ -2,16 +2,5 @@ from app.course_service_impl import CourseServiceImpl
 
 if __name__ == "__main__":
   course_service = CourseServiceImpl()
-  print(course_service.courses)
 
   # Start receiving requests...
-
-  course_service.create_course("CSCC73")
-  print(course_service.get_courses())
-  print(course_service.get_course_by_id(1))
-  print(course_service.delete_course(1))
-  print(course_service.get_courses())
-  course_service.create_course("CSCC73")
-  print(course_service.create_assignment(2, "Algorithms 1"))
-  print(course_service.get_courses())
-
